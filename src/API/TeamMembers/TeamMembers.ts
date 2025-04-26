21f9ff49d7fca0f51fe2ce14e135da738c218724
Ivan Ugnenko <vanchopi@yahoo.com>
2025-04-26 07:15:25 +0400

    BASE_URL fix

--- src/API/TeamMembers/TeamMembers.ts
+++ src/API/TeamMembers/TeamMembers.ts
@@ -4,17 +4,17 @@ export default class TeamMembersApi {
   _url: string;
 
   constructor() {
-    this._url = `/`;
+    this._url = process.env.BASE_URL;
   }
 
   get<T = any>(url: string): Promise<T> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      axios
-        .get(`${this._url}${url}`)
-        .then(({ data }) => resolve(data))
-        .catch((error) => reject(error));
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        axios
+          .get(`${this._url}${url}`)
+          .then(({ data }) => resolve(data))
+          .catch((error) => reject(error));
       }, 1000); // response delay imitation
-    })
+    });
   }
 }
